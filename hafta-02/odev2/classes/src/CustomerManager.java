f18b71b9b783289183b1e1808ff4e58556beaad8
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-10-08 21:33:25 +0300

    'classes' adde.

---
+++ hafta-02/odev2/classes/src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager {
+  public void Add() {
+    System.out.println("Musteri Eklendi.");
+  }
+
+  public void Remove() {
+    System.out.println("Musteri Kaldirildi.");
+  }
+
+  public void Update() {
+    System.out.println("Musteri Guncellendi.");
+  }
+}
