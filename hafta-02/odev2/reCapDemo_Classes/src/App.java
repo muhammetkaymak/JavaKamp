ef1cd3301c3836500239eb990e1ed20d94e602e7
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-10-08 21:41:00 +0300

    'reCapDemo_Classes' added.

---
+++ hafta-02/odev2/reCapDemo_Classes/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(4, 3);
+        System.out.println(sonuc);
+    }
+}
