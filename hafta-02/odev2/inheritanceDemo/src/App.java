57cd66ca16e2afdd48de71e8cff4d2f984b76acd
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-10-09 22:30:00 +0300

    'inheritanceDemo' added.

---
+++ hafta-02/odev2/inheritanceDemo/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        KrediUI krediUI = new KrediUI();
+        krediUI.KrediHesapla(new AskerKrediManager());
+    }
+}
