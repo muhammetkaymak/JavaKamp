25db64d8ca5d4bfdcb29f6f7a29aa205d7ae0081
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-10-09 20:42:25 +0300

    'methodOverloading' added.

---
+++ hafta-02/odev2/methodOverloading/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.topla(2, 3);
+        dortIslem.topla(1, 2, 3);
+    }
+}
