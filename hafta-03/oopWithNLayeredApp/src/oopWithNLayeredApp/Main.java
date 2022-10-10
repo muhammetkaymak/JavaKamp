1a82ec54509d75d25ef3251a11fbb6361cdd0ac0
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-10-10 22:49:56 +0300

    'oopWithNLayeredApp' added.

--- hafta-03/oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
+++ hafta-03/oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -1,13 +1,25 @@
 package oopWithNLayeredApp;
 
+// import java.lang.System.Logger;
+
 import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.core.logging.DatabaseLogger;
+import oopWithNLayeredApp.core.logging.FileLogger;
+import oopWithNLayeredApp.core.logging.MailLogger;
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.dataAccess.HibernateProductDao;
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.entities.Product;
 
 public class Main {
   public static void main(String[] args) throws Exception {
     Product product1 = new Product(1, "Iphone X", 10);
 
-    ProductManager productManager = new ProductManager();
+    Logger[] loggers = {new DatabaseLogger(), new FileLogger(), new MailLogger()};
+
+    ProductManager productManager = new ProductManager(new HibernateProductDao(),loggers);
     productManager.add(product1);
+
+
   }
 }
--- hafta-03/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ hafta-03/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,16 +1,35 @@
 package oopWithNLayeredApp.business;
 
+import java.lang.System.Logger;
+
+// import oopWithNLayeredApp.core.logging.Logger;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.dataAccess.ProductDao;
 import oopWithNLayeredApp.entities.Product;
 
 public class ProductManager {
+  private ProductDao productDao;
+  private Logger[] loggers;
+  
+  public ProductManager(Logger[] loggers) {
+    this.loggers = loggers;
+  }
+
+
+  public ProductManager(ProductDao productDao, oopWithNLayeredApp.core.logging.Logger[] loggers) {
+    this.productDao = productDao;
+  }
+
+
   public void add(Product product) throws Exception{
     //iş kuralları
     if(product.getUnitPrice()<10) {
       throw new Exception("Urun fiyati 10'dan kucuk olamaz.");
     }
-
-    JdbcProductDao productDao = new JdbcProductDao();
     productDao.add(product);
+
+    for ( Logger logger:loggers ) {
+      logger.log(null, product.getName());
+    }
   }
 }
