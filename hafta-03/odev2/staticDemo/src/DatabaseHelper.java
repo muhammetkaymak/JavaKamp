c5f0b6b38953b4d95dfcd31f8c499cf65987d617
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-10-12 23:51:29 +0300

    'staticDemo' added.

---
+++ hafta-03/odev2/staticDemo/src/DatabaseHelper.java
@@ -0,0 +1,14 @@
+public class DatabaseHelper {
+  public static class Crud {
+    // CRUD Create-Read-Update-Delete
+
+    public void Delete() {}
+    public void Update() {}
+  }
+
+  public static class Connection {
+    public static void createConnection() {
+      
+    }
+  }
+}
