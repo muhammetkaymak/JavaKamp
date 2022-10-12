1aa2a8dcbd9c99274c8b3b6024bdf8559e9095d1
muhammetkaymak <kaymakmuhammet52@gmail.com>
2022-10-12 21:20:01 +0300

    'abstractClasses' added.

---
+++ hafta-03/odev2/abstractClasses/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        WomanGameCalculator womanGameCalculator = new WomanGameCalculator();
+        womanGameCalculator.hesapla();
+        womanGameCalculator.gameOver();
+    }
+}
